22f82dd6fe82b8fd0957eeb85b3e0090dd4e3a90
nikolaia <nikolaia@users.noreply.github.com>
2015-05-28 20:55:05 +0200

    Reboot the project. Start with new Auth project and dnx beta5

--- src/DevOpsFeed.Auth/Startup.cs
+++ src/DevOpsFeed.Auth/Startup.cs
@@ -1,41 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Security.Claims;
+using Microsoft.AspNet.Authentication.Cookies;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
 
 namespace DevOpsFeed.Auth
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
-        {
-        }
-
-        // This method gets called by a runtime.
-        // Use this method to add services to the container
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
-            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
-            // services.AddWebApiConventions();
+            services.AddAuthentication();
         }
 
-        // Configure is called after ConfigureServices is called.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerfactory)
         {
-            // Configure the HTTP request pipeline.
-            app.UseStaticFiles();
+            loggerfactory.AddConsole(LogLevel.Information);
 
-            // Add MVC to the request pipeline.
-            app.UseMvc();
-            // Add the following route for porting Web API 2 controllers.
-            // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            app.UseCookieAuthentication(options =>
+            {
+                options.AutomaticAuthentication = true;
+            });
+
+            app.Run(async context =>
+            {
+                if (string.IsNullOrEmpty(context.User.Identity.Name))
+                {
+                    var user = new ClaimsPrincipal(new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, "bob") }));
+                    context.Authentication.SignIn(CookieAuthenticationDefaults.AuthenticationScheme, user);
+                    context.Response.ContentType = "text/plain";
+                    await context.Response.WriteAsync("Hello First timer");
+                    return;
+                }
+
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Hello old timer");
+            });
         }
     }
 }
---
+++ test/DevOpsFeed.Auth.Tests/AuthTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.TestHost;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using Xunit;
+
+namespace DevOpsFeed.Auth.Tests
+{
+    public class AuthTests
+    {
+        private TestServer CreateTestServer()
+        {
+            return TestServer.Create(app =>
+            {
+                var logging = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
+                new Startup().Configure(app, logging);
+            }, services =>
+            {
+                services.AddAuthentication();
+            });
+        }
+
+        [Fact]
+        public async void ValidUsernameAndPasswordShouldReturnToken()
+        {
+            var server = CreateTestServer();
+
+            var response = await server.CreateClient().GetAsync("/");
+            
+            Assert.Equal("Hello First timer", await response.Content.ReadAsStringAsync());
+        }
+    }
+}
