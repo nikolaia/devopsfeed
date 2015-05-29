872b57d3c08158041aa5888fc0d29f7cb15ad8fa
nikolaia <nikolai.andersen@bekk.no>
2015-05-29 16:19:31 +0200

    Use beta6. Use aspnet project custom xunit. Add Cookie tests

--- test/DevOpsFeed.Auth.Tests/AuthTests.cs
+++ test/DevOpsFeed.Auth.Tests/AuthTests.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNet.TestHost;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
 using Xunit;
@@ -20,13 +23,44 @@ namespace DevOpsFeed.Auth.Tests
         }
 
         [Fact]
-        public async void ValidUsernameAndPasswordShouldReturnToken()
+        public async void FirstRequestShouldReturnHelloFirstTimer()
         {
             var server = CreateTestServer();
 
             var response = await server.CreateClient().GetAsync("/");
             
             Assert.Equal("Hello First timer", await response.Content.ReadAsStringAsync());
+        }
+        
+        [Fact]
+        public async void SecondRequestShouldReturnHelloOldTimer()
+        {
+            var server = CreateTestServer();
+            var client = server.CreateClient();
+            
+            var initialResponse = await client.GetAsync("/");
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/");
+            var setCookie = initialResponse.Headers.GetValues("Set-Cookie").SingleOrDefault();
+            var cookieName = setCookie.Split(new[] { ';' }, 2).First();
+            
+            request.Headers.Add("Set-Cookie", setCookie);
+            request.Headers.Add("Cookie", cookieName);
+
+            var response = await client.SendAsync(request);
+            Assert.Equal("Hello old timer", await response.Content.ReadAsStringAsync());
+        }
+        
+        [Fact]
+        public async void SecondRequestShouldReturnHelloFirstTimerIfNotAuthorized()
+        {
+            var server = CreateTestServer();
+            var client = server.CreateClient();
+            
+            var initialResponse = await client.GetAsync("/");
+            var secondResponse = await client.GetAsync("/");
+            
+            Assert.Equal("Hello First timer", await secondResponse.Content.ReadAsStringAsync());
         }
     }
 }
